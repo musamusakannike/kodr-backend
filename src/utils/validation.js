fbe0129b6c76d97c52015ba827f1d638b604c8fb
Musa Musa Kannike <156730039+musamusakannike@users.noreply.github.com>
2024-12-23 15:21:22 +0100

    Added auth validation and model

---
+++ src/utils/validation.js
@@ -0,0 +1,23 @@
+const Joi = require("joi");
+
+const validateSignup = (data) => {
+  const schema = Joi.object({
+    username: Joi.string().alphanum().min(3).max(30).required(),
+    email: Joi.string().email().required(),
+    password: Joi.string().min(6).max(100).required(),
+    confirmPassword: Joi.string().min(6).max(100).required(),
+  });
+
+  return schema.validate(data);
+};
+
+const validateLogin = (data) => {
+  const schema = Joi.object({
+    email: Joi.string().email().required(),
+    password: Joi.string().min(6).max(100).required(),
+  });
+
+  return schema.validate(data);
+};
+
+module.exports = { validateSignup, validateLogin };
